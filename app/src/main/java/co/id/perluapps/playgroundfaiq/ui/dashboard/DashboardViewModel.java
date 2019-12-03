12dddd840dd22ee7ae0b0afe391f2551e7d85abf
faiqazmi9 <faiqazmi9@gmail.com>
2019-12-03 15:58:53 +0700

    initial commit

---
+++ app/src/main/java/co/id/perluapps/playgroundfaiq/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package co.id.perluapps.playgroundfaiq.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
